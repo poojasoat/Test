ea178e257f0c8be412b091984019cd676229bd93
pooja <poojasoat@gmail.com>
2020-05-04 13:12:06 +0530

    Html Test

---
+++ index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+
+
+</head>
+<body>
+<header>
+<div class="container-fluid px-0 ">
+<nav class="navbar navbar-expand-md navbar-dark pt-3   flex-row header-nav" style="background-color:#363d4d;">
+     
+        <a class="navbar-brand mr-0" id="infoToggler" href="/">
+          <img class="img-fluid logo mr-3 mb-4" src="images/app-logo.png"></a>
+         
+         <ul class="pl-2">
+    
+        <li class="list-unstyled text-light ">
+         <span>Axonator Demo</span>
+           <li class="nav-item dropdown list-unstyled ">
+                <a class="nav-link dropdown-toggle mr-3 mr-lg-0 text-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 V2.000<span class="caret"></span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="">User</a>
+                    <a class="dropdown-item" href="">Login</a>
+                </div>
+            </li>
+        </li>
+         
+       
+       </ul>
+        <ul class="navbar-nav flex-row mr-lg-0 ml-auto">
+            <li class="nav-item mr-3">
+                 <a href="#" class="text-light">V2.000 Is Pulished </a>
+            </li>
+            <li class="nav-item">
+                 <a href="#"> 
+            <button class="number-button1 serial mr-3 text-light" id="4">+</button></a>
+            </li>
+             <li class="nav-item">
+                 <a href="website-series.php#X"> 
+            <button class="number-button1  mr-3" id="4">
+            <i style='font-size:24px' class='far'>&#xf0f3;</i></button></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle mr-3 mr-lg-0 " id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="jk-icon"> Jk</span><span class="caret"></span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="">User</a>
+                    <a class="dropdown-item" href="">Login</a>
+                </div>
+            </li>
+        </ul>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    </nav>
+  
+</div>
+</header>
+
+<div class="container-fluid pl-0">
+  <div class="row">
+    <div class="col-md-2 pr-0">
+    <ul class="nav flex-column  h-100 pl-0 navigationmenu">
+    <li class="nav-item bg-secondary p-2">
+     <a class="nav-link " href="#">
+     <img src="images/my-apps-icon.png" class="mr-3"/>MY APPS</a>
+    </li>
+    <li class="nav-item bg-secondary p-1 ">
+      <a class="nav-link" href="#">BUILDER</a>
+    </li>
+    <li class="nav-item  bg-white">
+      <a class="nav-link active text-dark"  href="#" style="color:black !important;">
+      <img src="images/home@2x.png" class="imgs mr-3 "/>Home</a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link" href="#">
+      <img src="images/form-icon@2x.png" class="imgs mr-3"/>Forms</a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link" href="#">
+      <img src="images/workflow-icon@2x.png" class="imgs mr-3"/>Workflows</a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link" href="#">
+      <img src="images/users-icon@2x.png" class="imgs mr-3"/>Users</a>
+    </li>
+     <li class="nav-item mb-5">
+      <a class="nav-link" href="#">
+     <img src="images/form-icon@2x.png" class="imgs mr-3"/>Templates</a>
+    </li> 
+    <li class="nav-item bg-secondary p-1">
+      <a class="nav-link" href="#">Drive</a>
+    </li>
+ </li> <li class="nav-item">
+      <a class="nav-link" href="#">
+      <img src="images/tasks-icon@2x.png" class="imgs mr-3"/>Tasks</a>
+    </li>
+     </li> 
+     <li class="nav-item">
+      <a class="nav-link" href="#">
+      <img src="images/axo-data-icon@2x.png" class="imgs mr-3"/> Data</a>
+    </li>
+     <li class="nav-item mt-5">
+      <a class="nav-link" href="#">
+      <img src="images/logo@2x.png" class=" mr-3"/ style="    width: 95px;"> </a>
+    </li>
+    
+  </ul>
+    </div>
+    <div class="col-md-10 pl-0 dashoboard-area">
+    <nav class="navbar navbar-expand-sm " style="background-color:#f6f8ff !important;">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Dashboard</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">App Settings</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">App Logs</a>
+    </li>
+  </ul>
+</nav>
+<div class="container-fluid">
+  <div class="row mt-5">
+    <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+               <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-4 rideone">
+                    <img src="images/user-colored-icon.png" class="logo">
+                </div>
+                <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                    <h4>01</h4>
+                    <h2>Users on Field</h2>
+                </div>
+            </div>
+        </div>
+        </div>
+  </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+               <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-4 rideone">
+                    <img src="images/cost-colored-icon.png" class="logo">
+                </div>
+                <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                    <h4>$8</h4>
+                    <h2>Cost Saved</h2>
+                </div>
+            </div>
+        </div>
+        </div>
+  </div>
+    </div>
+     <div class="row mt-3">
+        <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+               <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-4 rideone">
+                    <img src="images/time-colored-icon.png" class="logo">
+                </div>
+                <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                    <h4>17</h4>
+                    <h2>Minuites Saved</h2>
+                </div>
+            </div>
+        </div>
+        </div>
+  </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+               <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-4 rideone">
+                    <img src="images/records-colored-icon.png" class="logo">
+                </div>
+                <div class="col-lg-9 col-md-8 col-sm-8 col-8 fontsty">
+                    <h4>04</h4>
+                    <h2>Records Submitted</h2>
+                </div>
+            </div>
+        </div>
+        </div>
+  </div>
+    </div>
+     <div class="row mt-3">
+        <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+               <div class="row">
+                <div class="col-md-6">
+                <h6>Overall Field Performance</h6>
+                <p>Improve your performance
+                <ol>
+                <li>Engage your performance</li>
+                <li>Keep the content simple and to the port</li>
+                <li>Tag your images appority</li>
+                </ol>
+                </div>
+                 <div class="col-md-6">
+                 <p class="text-center">50%</p>
+                 <div class="progress-bar">
+                      <div class="barOverflow">
+                        <p class="display-progress mt-4 mb-0">Fair</p>
+                        <p class="display-progress">1 Hr 20 Min</p>
+
+                        <div class="bar"></div>
+
+                      </div>
+                      <span>50%</span>
+                    </div>
+                      <p class="poor">Poor</p>
+                      <p class="good">Good</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+</div>
+  
+  </div>
+      <div class="col-md-6">
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.2134702143876!2d73.77153171436876!3d18.519253074049125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2be5a2517e53f%3A0x271c5745edb54212!2sWhiteSnow%20Software%20Consultancy%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1588576192342!5m2!1sen!2sin" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</div>
+ <div class="container-fluid mt-5">
+    <div class="row">
+      <div class="col-md-4">
+      <div class="card p-3">
+            <h6 class="p-3">Student Information</h6>
+
+        <div class="card-body">
+      <div class="row">
+        <div class="col-md-6">
+           <div class="pieContainer">
+              <div class="pieBackground">
+                <p>80%</p>
+                <p class="percenteage">50%</p>
+                <p class="percenteage1">20%</p>
+              </div>
+              <div id="pieSlice2" class="hold">
+              <div class="pie">
+              </div></div>
+              <div id="pieSlice3" class="hold">
+              <div class="pie"></div>
+             
+              </div>
+              <div id="pieSlice6" class="hold"><div class="pie"></div></div>
+              <div class="innerCircle"><div class="content"></div></div>
+        </div>
+      </div>
+      <div class="col-md-6">
+      <ul class="choice">
+        <li>Choice 4</li>
+        <li>Choice 3</li>
+        <li>Choice 2</li>
+        <li>Choice 1</li>
+      </ul>
+    </div>
+      </div>
+      </div>
+      </div>
+</div>
+      <div class="col-md-4">
+      <div class="card p-3">
+        <div class="card-body">
+                    <h6 class="p-3">Teacher Information</h6>
+
+      <div class="row">
+        <div class="col-md-6">
+           <div class="pieContainer">
+             <div class="pieBackground">
+                <p>80%</p>
+                <p class="percenteage">50%</p>
+                <p class="percenteage1">20%</p>
+              </div>
+              <div id="pieSlice2" class="hold">
+              <div class="pie"></div></div>
+              <div id="pieSlice3" class="hold">
+              <div class="pie"></div>
+              <div><p>50%</p></div>
+              </div>
+              <div id="pieSlice6" class="hold"><div class="pie"></div></div>
+              <div class="innerCircle"><div class="content"></div></div>
+        </div>
+      </div>
+      <div class="col-md-6">
+       <ul class="choice">
+        <li>Choice 4</li>
+        <li>Choice 3</li>
+        <li>Choice 2</li>
+        <li>Choice 1</li>
+      </ul>
+      </div>
+      </div>
+      </div>
+      </div>
+</div>
+       <div class="col-md-4">
+      <div class="card">
+        <div class="card-body p-3">
+                    <h6 class="p-3">Educational Information</h6>
+
+      <div class="row">
+        <div class="col-md-6">
+           <div class="pieContainer">
+           <div class="pieBackground">
+                <p>80%</p>
+                <p class="percenteage">50%</p>
+                <p class="percenteage1">20%</p>
+              </div>
+              <div id="pieSlice2" class="hold">
+              <div class="pie"></div></div>
+              <div id="pieSlice3" class="hold">
+              <div class="pie"></div>
+              <div><p>50%</p></div>
+              </div>
+              <div id="pieSlice6" class="hold"><div class="pie"></div></div>
+              <div class="innerCircle"><div class="content"></div></div>
+        </div>
+      </div>
+      <div class="col-md-6">
+     <ul class="choice">
+        <li>Choice 4</li>
+        <li>Choice 3</li>
+        <li>Choice 2</li>
+        <li>Choice 1</li>
+      </ul>
+      </div>
+      </div>
+      </div>
+      </div>
+</div>
+    </div>
+  </div>
+  </div>
+ 
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript">
+  $("#infoToggler").click(function() {
+    $(this).find('img').toggle();
+});
+</script>
+<script type="text/javascript">
+  $(".progress-bar").each(function(){
+  
+  var bar = $(this).find(".bar");
+  var val = $(this).find("span");
+  var per = parseInt( val.text(), 10);
+
+  $({p:0}).animate({p:per}, {
+    duration: 3000,
+    easing: "swing",
+    step: function(p) {
+      bar.css({
+        transform: "rotate("+ (5+(p*1.8)) +"deg)"
+      });
+      val.text(p|0);
+    }
+  });
+});
+</script>
+</body>
+</html>
+
+
